83d8d50908c5a1c6aa976a1cc1f4dc7578722a0c
ekdrhaod <201311281@sangmyung.kr>
2016-11-22 21:19:29 +0900

    First Commit
    첫경험 ~

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    // public Rigidbody rb;
+    // public GameObject fv;
+    public float speed = -5f;
+    
+    void Start()
+    {
+       // rb = GetComponent<Rigidbody>();
+        
+    }
+    void Update()
+    {
+        transform.Translate(speed*Time.deltaTime, 0, 0);
+       
+    }
+
+   // void FixedUpdate()
+    //{
+        
+         
+
+   // }
+}
